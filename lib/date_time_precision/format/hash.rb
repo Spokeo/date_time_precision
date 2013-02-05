1016938c394182bb22bc4d03e80be35f240f12cc
David Butler <dwbutler@ucla.edu>
2013-02-05 12:29:27 -0800

    Accept string or symbol hash keys

--- lib/date_time_precision/format/hash.rb
+++ lib/date_time_precision/format/hash.rb
@@ -21,13 +21,13 @@ class Hash
   
   protected
   def date_time_args
-    [self[:year] || self[:y],
-    self[:mon] || self[:m] || self[:month],
-    self[:mday] || self[:d] || self[:day],
-    self[:hour] || self[:h],
-    self[:min],
-    self[:sec],
-    self[:sec_frac]]
+    [self[:year] || self[:y] || self['year'] || self['y'],
+    self[:mon] || self[:m] || self[:month] || self['mon'] || self['m'] || self['month'],
+    self[:mday] || self[:d] || self[:day] || self['mday'] || self['d'] || self['day'],
+    self[:hour] || self[:h] || self[:hr] || self['hour'] || self['h'] || self['hr'],
+    self[:min] || self['min'],
+    self[:sec] || self[:s] || self['sec'] || self['s'],
+    self[:sec_frac] || self['sec_frac']]
   end
 end
 
