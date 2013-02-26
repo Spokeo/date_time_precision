5203cb865599a10d296413b7bedaaa607bd97c3a
David Butler <dwbutler@ucla.edu>
2013-02-26 09:25:38 -0800

    Ruby 2.0.0 compatibility

--- lib/date_time_precision/patch.rb
+++ lib/date_time_precision/patch.rb
@@ -6,6 +6,9 @@ DateTimePrecision::PATCH_VERSION = begin
   if defined?(JRUBY_VERSION) || (defined?(RUBY_ENGINE) and RUBY_ENGINE == 'rbx')
     #JRuby and Rubinius implement the Date/Time classes in pure Ruby, so they can use the 1.9.2 patch
     RUBY_VERSION >= '1.9' ? '1.9.2' : '1.8.7'
+  elsif RUBY_VERSION > '2'
+    # The 1.9.3 patch works in Ruby 2.0.0
+    '1.9.3'
   else
     RUBY_VERSION
   end
